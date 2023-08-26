6953cdd0a744a7516a019a1382518a9e912ec8a4
NirvanaAkisa <137563355+NirvanaAkisa@users.noreply.github.com>
2023-08-26 22:18:20 +0200

    Updated forecast functions

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ function formatDay(timestamp) {
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Sun", "Mon", "Tue", "Wed"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
 
   let forecastHTML = `<div class="row">`;
 
@@ -57,7 +57,16 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
+   
 }
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "1266ad07b66517497b1acf79ea5a6a64";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -78,13 +87,14 @@ function displayTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
   let apiKey = "1266ad07b66517497b1acf79ea5a6a64";
   let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Cape Town&appid=${apiKey}&units=${units}`;
-
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
